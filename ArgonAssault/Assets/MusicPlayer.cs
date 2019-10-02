e009da9d4d0fd03ecb58fcea84b02a22811f18fb
Justin <justindpnt@gmail.com>
2019-10-01 21:38:57 -0700

    Added player ship and basic camera pathing

---
+++ ArgonAssault/Assets/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Music should persist through scenes
+    private void Awake() 
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    // Manager to load the main game
+    void LoadFirstScene() 
+    {
+        SceneManager.LoadScene(1);
+    }
+}
