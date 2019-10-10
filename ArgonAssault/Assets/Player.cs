e888957608005085dc21b9f88a7741a47f98fdf6
Justin <justindpnt@gmail.com>
2019-10-09 21:05:27 -0700

    RL Player movement

--- ArgonAssault/Assets/Player.cs
+++ ArgonAssault/Assets/Player.cs
@@ -5,6 +5,10 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    //Cached references
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4000f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        transform.localPosition = new Vector3(rawXPos, transform.localPosition.y, transform.localPosition.z);
     }
 }
