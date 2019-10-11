b78c12f42d76673ad1a3d05391acc9a30b017943
Justin <justindpnt@gmail.com>
2019-10-10 19:04:44 -0700

    Added vertical movement

--- ArgonAssault/Assets/Player.cs
+++ ArgonAssault/Assets/Player.cs
@@ -6,8 +6,9 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     //Cached references
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4000f;
+    [Tooltip("In ms^-1")][SerializeField] float Speed = 4000f;
     [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,16 @@ public class Player : MonoBehaviour
     void Update()
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float xOffset = xThrow * Speed * Time.deltaTime;
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * Speed * Time.deltaTime;
 
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
-        transform.localPosition = new Vector3(rawXPos, transform.localPosition.y, transform.localPosition.z);
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
