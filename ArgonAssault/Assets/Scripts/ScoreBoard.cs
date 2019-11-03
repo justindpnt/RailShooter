150cb0080f50b9ed7870205ee9924cf5d04f727e
Justin <justindpnt@gmail.com>
2019-11-03 12:39:54 -0800

    Changed text score to textMeshPro

---
+++ ArgonAssault/Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+//To enable the score functionality
+public class ScoreBoard : MonoBehaviour
+{
+    //Same score no matter the enemy
+    [SerializeField] int scorePerHit = 12;
+
+    int score = 0;
+    TMP_Text textScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textScore = GetComponent<TextMeshProUGUI>();
+        textScore.text = score.ToString();
+    }
+
+    public void ScoreHit() 
+    {
+        score = score + scorePerHit;
+        textScore.text = score.ToString();
+    }
+}
