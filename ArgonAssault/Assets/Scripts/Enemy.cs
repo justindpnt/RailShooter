754b0a9d3b5b3ba58f239e2f1232e9766a29868b
Justin <justindpnt@gmail.com>
2019-11-04 18:57:52 -0800

    Added enemy health

--- ArgonAssault/Assets/Scripts/Enemy.cs
+++ ArgonAssault/Assets/Scripts/Enemy.cs
@@ -9,6 +9,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 12;
+    [SerializeField] int hits = 10;
 
     ScoreBoard scoreBoard;
 
@@ -26,11 +27,21 @@ public class Enemy : MonoBehaviour
     }
 
     //Handle the collison between the gun rays and the enemy ship
-    void OnParticleCollision(GameObject other) 
+    void OnParticleCollision(GameObject other)
     {
-        scoreBoard.ScoreHit(scorePerHit);
+        hits--;
+        if(hits <= 0)
+        {
+            scoreBoard.ScoreHit(scorePerHit);
+            KillEnemy();
+        }
+    }
+
+    //Generate explosion and remove enemy
+    private void KillEnemy()
+    {
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
-        Destroy(gameObject);   
+        Destroy(gameObject);
     }
 }
